f63ce06cfbc1ec6e0c3e06011d83465c97090e3c
Leonardo Mosciatti <leonardo.mosciatti@gmail.com>
2017-09-10 12:55:20 +0200

    Working prototype.

--- simplefs.c
+++ simplefs.c
@@ -190,7 +190,7 @@ int simplefs_update_file_node(struct simplefs_node *node, char *content) {
     if( (node->file_content = strdup(content)) == NULL) {
         return 0;
     }
-    return 1;
+    return strlen(content);
 }
 
 int simplefs_add_group_to_node(struct simplefs_node *node,
@@ -340,10 +340,139 @@ struct simplefs_node *simplefs_create_dir(struct simplefs_node *root, char *path
     if(simplefs_add_group_to_node(new_node, new_group) == 0) {
         return NULL;
     }
-
     return new_node;
 }
 
+char *simplefs_read(struct simplefs_node *root, char *path) {
+    struct simplefs_node *target = NULL;
+
+    target = simplefs_walk_path(root, path);
+
+    if(target == NULL) {
+        return NULL;
+    }
+
+    return target->file_content;
+}
+
+int simplefs_write(struct simplefs_node *root, char *path, char *content) {
+    struct simplefs_node *target = NULL;
+    
+    target = simplefs_walk_path(root, path);
+
+    if(target == NULL) {
+        return 0;
+    }
+
+    return simplefs_update_file_node(target, content);
+}
+
+int simplefs_get_free_sons_for_node(struct simplefs_node *node) {
+    struct simplefs_node_group *iterator;
+    int free_sons = 0;
+        
+    if(node->dir == NULL) {
+        if(node->file_content != NULL) { 
+            return -1;
+        } else {
+            return -2;
+        }
+    }
+
+    iterator = node->dir;
+
+    while(iterator != NULL) {
+        free_sons += iterator->free_sons;
+        iterator = iterator->next;
+    }
+
+    return free_sons + ((GROUPS_PER_NODE - node->number_of_groups) * SONS_PER_GROUP);
+}
+
+int simplefs_delete_node(struct simplefs_node *node) {
+    struct simplefs_node_group *iterator = NULL;
+    int i = 0;
+    int free_sons = simplefs_get_free_sons_for_node(node); 
+    
+    if(free_sons < 0 || free_sons == MAX_SONS_PER_NODE) {
+        /*
+         * freeing target from parent.
+         */
+        iterator = node->parent->dir;
+        while(iterator != NULL) {
+            for(i = 0; i < SONS_PER_GROUP; i++) {
+                if((iterator->sons)[i] != NULL) {
+                    if(strcmp((iterator->sons)[i]->name, node->name)==0) {
+                        (iterator->sons)[i] = NULL;
+                        break;
+                    }
+                }
+            }
+            iterator = iterator->next;
+        }
+        
+        if(free_sons == -1) { 
+            free(node->file_content); 
+            return 1;
+        }
+        
+        if(node->dir != NULL) {
+            iterator = node->dir;
+            while(iterator != NULL) {
+                iterator = iterator->next;
+                free(node->dir);
+                node->dir = iterator;
+            }
+        }
+        free(node);
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int simplefs_delete_node_recursive(struct simplefs_node *node) {
+    struct simplefs_node_group *iterator = NULL;
+    struct simplefs_node *tmp = NULL;
+    int i = 0; 
+
+    iterator = node->dir; 
+    while(iterator != NULL) {
+        for(i = 0; i < SONS_PER_GROUP; i++) {
+            if((iterator->sons)[i] != NULL) {
+                if(simplefs_delete_node((iterator->sons)[i]) == 0) {
+                    return simplefs_delete_node_recursive((iterator->sons)[i]);
+                } 
+            }
+        }
+        iterator = iterator->next;
+    }
+}
+
+int simplefs_delete(struct simplefs_node *root, char *path) {
+    struct simplefs_node *target = NULL;
+
+    target = simplefs_walk_path(root, path);
+
+    if(target == NULL) {
+        return 0;
+    }
+
+    return simplefs_delete_node(target);
+}
+
+int simplefs_delete_recursive(struct simplefs_node *root, char *path) {
+    struct simplefs_node_group *iterator = NULL;
+    struct simplefs_node *tmp = NULL;
+    int i = 0; 
+    char *current_node_path;
+
+    tmp = simplefs_walk_path(root, path); 
+    if(tmp == NULL) {
+       return 0;
+    }
+    return simplefs_delete_node_recursive(tmp);
+}
 /*
  * Parsing a line representing a command.
  * The first argument is the line to be parsed; 
@@ -382,21 +511,52 @@ int simplefs_parse_command_line(char *line, char **command_name, char **first_ar
 
 int simplefs_execute_command( struct simplefs_node *root, char *command_name, char *first_argument, char *second_argument) {
     if(strcmp("create", command_name) == 0){
-        printf("create called! %s\n", first_argument);
+        if(simplefs_create_node(root, first_argument) != NULL) {
+            simplefs_write(root, first_argument, "");
+            printf("ok\n");
+        } else {
+            printf("no\n");
+        }
     } else if(strcmp("create_dir", command_name) == 0){
-        printf("create_dir called!\n");    
+        if(simplefs_create_dir(root, first_argument) != NULL) {
+            printf("ok\n");
+        } else {
+            printf("no\n");
+        }
     } else if(strcmp("read", command_name) == 0){
-        printf("read called!\n");    
+        char *content = NULL;
+        content = simplefs_read(root, first_argument);
+        if(content == NULL) {
+            printf("no\n");
+        } else {
+            printf("contenuto %s\n", content);
+        }
     } else if(strcmp("write", command_name) == 0){
-        printf("write called!\n");    
+        second_argument++;
+        *(second_argument + strlen(second_argument) - 1) = '\0';
+        int written_chars = simplefs_write(root, first_argument, second_argument);
+        
+        if(written_chars == 0) {
+            printf("no\n");
+        } else {
+            printf("ok %d\n", written_chars);
+        }
     } else if(strcmp("delete", command_name) == 0){
-        printf("delete called!\n");    
+        if(simplefs_delete(root, first_argument) == 0) {
+            printf("no\n");
+        } else {
+            printf("ok\n");
+        }
     } else if(strcmp("delete_r", command_name) == 0){
-        printf("delete_r called!\n");    
+        if(simplefs_delete_recursive(root, first_argument) == 0) {
+            printf("no\n");
+        } else {
+            printf("ok\n");
+        }
     } else if(strcmp("find", command_name) == 0){
         printf("find called!\n");    
     } else if(strcmp("exit", command_name) == 0){
-        printf("exit called!\n");    
+        exit(EXIT_SUCCESS);
     }
 }
 
@@ -411,8 +571,6 @@ int main() {
     
     int number_of_arguments = 0;
 
-    //printf("%s\n", split_path("/")[0]);
-    
 
     struct simplefs_node *root;
     root = simplefs_init_node(NULL, "root", NULL, NULL);
@@ -421,14 +579,51 @@ int main() {
     root_dir = simplefs_init_node_group(root, NULL);
     simplefs_add_group_to_node(root, root_dir);
 
-
+    int i = 1;
+    
+    while ((read = getline(&line, &len, stdin)) != -1) {
+        number_of_arguments = simplefs_parse_command_line(line, &command_name, &first_argument, &second_argument);
+        simplefs_execute_command(root, command_name, first_argument, second_argument); 
+        command_name = NULL;
+        first_argument = NULL;
+        second_argument = NULL;
+        i++;
+    }
+    printf("\n");
+    if (line)
+        free(line);
+    
+    exit(EXIT_SUCCESS);
+}
+/*
     simplefs_create_dir(root, "/a");
-    simplefs_create_node(root, "/a/file1");
-    
-    
+    //simplefs_create_node(root, "/a/file1");
 
-    printf("%s", simplefs_walk_path(root, "/a/file1")->name);
+    int j = 0;
+    char b[10];
+    int r = 0;
+    for(j=0;j<1029;j++){
+        snprintf(b, 10, "/a/j%d", j); 
+        if(j<1024) assert(simplefs_create_node(root, b)!=NULL);
+        else assert(simplefs_create_node(root, b)==NULL);
+        printf("%d:\t%d\t%d\n", j, simplefs_get_free_sons_for_node(root),root->number_of_groups);
+        simplefs_write(root, b, "file di prova");
+        
+        //        assert(simplefs_walk_path(root, b) == NULL);
+    }    
 
+    simplefs_write(root, "/a/j2", "file di prova");
+    //assert(simplefs_write(root, "/a/file1", "ciaociao")!=0); 
+
+    //printf("%s\n", simplefs_read(root, "/a/file1"));
+    //
+    simplefs_create_dir(root, "/b");
+    simplefs_create_node(root, "/b/b1");
+    //simplefs_create_node(root, "/b/b2");
+    //simplefs_create_node(root, "/b/b3");
+    //simplefs_create_node(root, "/b/b4");
+    printf("%d\n", simplefs_delete_recursive(root, "/a"));
+    printf("%s\n", simplefs_read(root, "/a/j300"));
     int i = 0;
     char s[255][255];
     int depth = 0;
@@ -495,17 +690,4 @@ int main() {
     simplefs_add_new_group_to_node(root);
     printf("%d %d\n", root->dir->free_sons, root->number_of_groups);*/
 
-    /*
-    while ((read = getline(&line, &len, stdin)) != -1) {
-        number_of_arguments = simplefs_parse_command_line(line, &command_name, &first_argument, &second_argument);
-        simplefs_execute_command(root, command_name, first_argument, second_argument); 
-        command_name = NULL;
-        first_argument = NULL;
-        second_argument = NULL;
-    }
 
-    if (line)
-        free(line);
-    */
-    exit(EXIT_SUCCESS);
-}
